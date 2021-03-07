56fa7af2ebce9157f544c45aa68751426a24d516
Savannah Mann <mannnnnn@users.noreply.github.com>
2021-03-06 18:06:20 -0600

    Drill Part 2
    This... doesn't work but it is a step forwards

---
+++ Assets/MineLine.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineLine : MonoBehaviour
+{
+    //interaction logic
+    private MineableObject parent;
+    public bool interacting = false;
+    private Excavator.ControlMode cursorTool = Excavator.ControlMode.HAND;
+
+    void Start()
+    {
+        parent = transform.GetComponentInParent<MineableObject>();
+        if(parent == null)
+        {
+            UnityEngine.Debug.Log("MineLine isn't attached to a mineable object.");
+        }
+    }
+
+
+    public void OnMouseDown()
+    {
+        UnityEngine.Debug.Log("Hello!");
+        cursorTool = Excavator.GetInstance().controlMode;
+        interacting = true;
+        parent.UpdateDebugLabels();
+    }
+
+    public void OnMouseUp()
+    {
+        interacting = false;
+    }
+
+    void OnMouseExit()
+    {
+        interacting = false;
+    }
+
+    void OnMouseOver()
+    {
+        UnityEngine.Debug.Log("Oh!");
+        if (Input.GetMouseButton(0))
+        {
+            cursorTool = Excavator.GetInstance().controlMode;
+            interacting = true;
+        }
+        parent.UpdateDebugLabels();
+    }
+
+    void FixedUpdate()
+    {
+        if (cursorTool == Excavator.ControlMode.DRILL && interacting && !parent.broken)
+        {
+            UnityEngine.Debug.Log("Brrrr!");
+            //progress!
+            parent.progress += 0.01f;
+            if (parent.progress >= parent.completionThreshold)
+            {
+                parent.removeable = true;
+            }
+            parent.UpdateDebugLabels();
+        }
+    }
+}
--- Assets/MineableObject.cs
+++ Assets/MineableObject.cs
@@ -16,11 +16,11 @@ public class MineableObject : MonoBehaviour
 
     public Type type = Type.DEFAULT;
     private float quality = 1; //100%  //the score for this piece
-    private float progress = 0; //0%  //how far into the successful excavation you are
-    private float completionThreshold = 0.80f; //80% //when this piece can be removed
+    public float progress = 0; //0%  //how far into the successful excavation you are
+    public float completionThreshold = 0.80f; //80% //when this piece can be removed
     private float failureThreshold = 0f; //0% //when this piece becomes broken
-    private bool removeable = false;
-    private bool broken = false;
+    public bool removeable = false;
+    public bool broken = false;
     private float fragility = 0.01f; // how quickly an object breaks
 
     //interaction logic
@@ -41,7 +41,6 @@ public class MineableObject : MonoBehaviour
 
     public void OnMouseDown()
     {
-        UnityEngine.Debug.Log("Hello there!");
         cursorTool = Excavator.GetInstance().controlMode;
 
         if (cursorTool == Excavator.ControlMode.HAND)
@@ -64,12 +63,22 @@ public class MineableObject : MonoBehaviour
         interacting = false;
     }
 
+    void OnMouseExit()
+    {
+        interacting = false;
+    }
+
     void OnMouseOver()
     {
+        if (Input.GetMouseButton(0))
+        {
+            cursorTool = Excavator.GetInstance().controlMode;
+            interacting = true;
+        }
         UpdateDebugLabels();
     }
 
-    void UpdateDebugLabels()
+    public void UpdateDebugLabels()
     {
         chimera.currNameLabel.text = gameObject.name;
         chimera.currProgressLabel.text = broken ? "BROKEN" : "Progress: " + progress + "%";
@@ -83,7 +92,6 @@ public class MineableObject : MonoBehaviour
     {
         if(cursorTool == Excavator.ControlMode.DRILL && interacting && !broken)
         {
-            UnityEngine.Debug.Log("Drilling!");
             //damage has occurred!
             //TODO: bad vfx+sfx
             quality -= fragility;
