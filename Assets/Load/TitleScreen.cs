ebf370064e27b21d9cac330ef281e76cfe4b71b8
Savannah Mann <mannnnnn@users.noreply.github.com>
2021-04-12 23:14:24 -0500

    Title Screen Setup
    Stole the title screen I made for Aloft and set it up over here. Simple panels and a slideout volume bar.
    
    Also brought over the script that forces you to load in from a screen, and the nice button prefabs. Prefabs should be polished and probably reduced down to 1 button, and the script /probably/ isn't necessary for a singleplayer game, but I wanted to have it anyways

---
+++ Assets/Load/TitleScreen.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{
+
+    public GameObject VolumeMenu;
+    bool VolumeMenuShown = false;
+
+    void Start()
+    {
+
+        //Setup player prefs for volume if this is teh first time playing
+        if(!PlayerPrefs.HasKey("Volume"))
+        {
+            PlayerPrefs.SetFloat("Volume", 0.5f);
+        }
+        if (!PlayerPrefs.HasKey("Muted"))
+        {
+            PlayerPrefs.SetInt("Muted", 0);
+        }
+    }
+
+
+    public void ToggleVolumeMenu()
+    {
+        if (VolumeMenuShown)
+        {
+            VolumeMenu.GetComponent<Animator>().SetTrigger("Hide");
+        } else
+        {
+            VolumeMenu.GetComponent<Animator>().SetTrigger("Show");
+        }
+        VolumeMenuShown = !VolumeMenuShown;
+    }
+
+    public void IncreaseVolume()
+    {
+        if (PlayerPrefs.GetFloat("Volume") < 1)
+        {
+            PlayerPrefs.SetFloat("Volume", PlayerPrefs.GetFloat("Volume") + 0.1f);
+        }
+           
+    }
+
+    public void DecreaseVolume()
+    {
+        if (PlayerPrefs.GetFloat("Volume") > 0)
+        {
+            PlayerPrefs.SetFloat("Volume", PlayerPrefs.GetFloat("Volume") - 0.1f);
+        }
+    }
+
+    public void ToggleMute()
+    {
+        if (PlayerPrefs.GetInt("Mute") > 0)
+        {
+            PlayerPrefs.SetInt("Mute", 0);
+        } else
+        {
+            PlayerPrefs.SetInt("Mute", 1);
+        }
+    }
+
+    public void GoToArcadeScreen()
+    {
+        SceneManager.LoadScene("ArcadeScreen");
+    }
+}
